b6662f84d84b07af68d16acabb18cd24688f8e8a
Alejandraarrieta <arrieta1745@gmail.com>
2022-05-17 16:28:32 -0300

    solo-std

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+import (
+	"fmt"
+	"net/http"
+)
+
+func main(){
+	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request)  {
+
+		if r.Method != http.MethodGet {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			fmt.Fprintf(w, "Method not allowed\n")
+			return
+		}
+		fmt.Fprintf(w,"Hello there %s\n", "visitor")		
+	}) 
+
+	srv := http.Server {
+		Addr: ":8080",
+	}
+
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		panic(err)
+	}
+
+}
