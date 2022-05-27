bd48e2208e2c3edfb6756f53fab8a6a198404ede
Alejandraarrieta <arrieta1745@gmail.com>
2022-05-27 10:36:14 -0300

    port

--- main.go
+++ main.go
@@ -1,22 +1,23 @@
 package main
+
 import (
 	"fmt"
 	"net/http"
 )
 
-func main(){
-	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request)  {
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
 		if r.Method != http.MethodGet {
 			w.WriteHeader(http.StatusMethodNotAllowed)
 			fmt.Fprintf(w, "Method not allowed\n")
 			return
 		}
-		fmt.Fprintf(w,"Hello there %s\n", "visitor")		
-	}) 
+		fmt.Fprintf(w, "Hello there %s\n", "visitor")
+	})
 
-	srv := http.Server {
-		Addr: ":8080",
+	srv := http.Server{
+		Addr: ":9000",
 	}
 
 	err := srv.ListenAndServe()
